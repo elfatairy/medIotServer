721598d262156951fde39ef24261564de2951017
OmarHassan <omar13102005@gmail.com>
2025-01-06 13:50:21 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+const port = parseInt(process.env.PORT || "3000");
+
+app.use(bodyParser.json());
+app.disable("x-powered-by");
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.get('/', (req: Request, res: Response) => {
+    res.json({ 'message': 'ok' });
+})
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
