8055d5a671dafd3269fd86b7daa70beba3a096c6
OmarHassan <omar13102005@gmail.com>
2025-01-06 14:14:27 +0200

    sths

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+const port = parseInt(process.env.PORT || "3000");
+app.use(body_parser_1.default.json());
+app.disable("x-powered-by");
+app.use(body_parser_1.default.urlencoded({
+    extended: true,
+}));
+app.get('/', (req, res) => {
+    res.json({ 'message': 'ok' });
+});
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+});
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
